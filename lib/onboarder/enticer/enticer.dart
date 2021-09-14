a5428c82b599ce3f6180be1a2e615cc1e92309d0
lewcianci <lewcianci@hotmail.com>
2021-09-14 16:36:24 +1000

    onboarding process works

--- lib/onboarder/enticer/enticer.dart
+++ lib/onboarder/enticer/enticer.dart
@@ -1,16 +1,25 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class EnticerOpenerPage extends StatefulWidget {
-  const EnticerOpenerPage({Key? key}) : super(key: key);
+  final VoidCallback getStartedPressed;
+
+  const EnticerOpenerPage({
+    Key? key,
+    required this.getStartedPressed,
+  }) : super(key: key);
 
   @override
   _EnticerOpenerPageState createState() => _EnticerOpenerPageState();
 }
 
 class _EnticerOpenerPageState extends State<EnticerOpenerPage> {
-  int index = 0;
+  final List<String> textOpeners = ['HELLO', 'WELCOME', 'TO', 'SYNERGY', 'HELLO,\r\nWELCOME\r\nTO\r\nSYNERGY'];
+
+  int index = 1;
+  bool showStartCard = false;
 
   @override
   void initState() {
@@ -18,8 +27,11 @@ class _EnticerOpenerPageState extends State<EnticerOpenerPage> {
       Duration(seconds: 2),
       (timer) {
         setState(() {
-          if (index == 4) {
-            print('show the other one');
+          if (index == 5) {
+            timer.cancel();
+            setState(() {
+              showStartCard = true;
+            });
           } else {
             index++;
           }
@@ -33,18 +45,111 @@ class _EnticerOpenerPageState extends State<EnticerOpenerPage> {
   @override
   Widget build(BuildContext context) {
     return AnimatedSwitcher(
-        transitionBuilder: AnimatedSwitcher.defaultTransitionBuilder,
-        duration: const Duration(milliseconds: 400),
-        child: IndexedStack(
-          alignment: Alignment.center,
-          key: ValueKey<int>(index),
-          index: index,
+      switchInCurve: Curves.fastOutSlowIn,
+      transitionBuilder: (widget, animation) {
+        final zoomAnimation = Tween(begin: 1.0, end: 1.3).animate(animation);
+        final fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(
+          CurvedAnimation(
+            parent: animation,
+            curve: Interval(0.0, 0.2, curve: Curves.ease),
+          ),
+        );
+        return FadeTransition(
+          opacity: fadeAnimation,
+          child: ScaleTransition(
+            scale: zoomAnimation,
+            child: widget,
+          ),
+        );
+      },
+      duration: const Duration(milliseconds: 2000),
+      child: Container(
+        child: Stack(
           children: [
-            Image.asset('assets/opener/slide1.jpg'),
-            Image.asset('assets/opener/slide2.jpg'),
-            Image.asset('assets/opener/slide3.jpg'),
-            Image.asset('assets/opener/slide4.jpg'),
+            Center(
+              child: Text(
+                textOpeners[index - 1],
+                style: Theme.of(context).textTheme.headline3!.copyWith(
+                      fontWeight: FontWeight.w900,
+                      // color: Colors.white,
+                    ),
+              ),
+            ),
+            if (index == 5)
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 400),
+                opacity: showStartCard ? 1 : 0,
+                child: Align(
+                  child: Padding(
+                    padding: const EdgeInsets.all(80.0).copyWith(bottom: 120),
+                    child: BottomTextInvite(
+                      getStartedPressed: widget.getStartedPressed,
+                    ),
+                  ),
+                  alignment: Alignment.bottomCenter,
+                ),
+              )
           ],
-        ));
+        ),
+        key: ValueKey<int>(index),
+        height: double.maxFinite,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'assets/opener/slide${index}.jpg',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomTextInvite extends StatelessWidget {
+  final VoidCallback getStartedPressed;
+  BottomTextInvite({
+    Key? key,
+    required this.getStartedPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // color: Theme.of(context).cardColor.withOpacity(0.7),
+      // shadowColor: Colors.black.withOpacity(0.3),
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Text("Prepare to holiday"),
+            Text.rich(
+              TextSpan(
+                style: Theme.of(context).textTheme.headline6,
+                children: [
+                  TextSpan(text: 'Prepare to holiday'),
+                  TextSpan(
+                    text: ' better.',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  )
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                getStartedPressed();
+              },
+              child: Text('GET STARTED'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/onboarder/onboarder.dart
+++ lib/onboarder/onboarder.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:onboarder/onboarder/enticer/enticer.dart';
@@ -10,71 +11,286 @@ class OnboarderPage extends StatefulWidget {
   _OnboarderPageState createState() => _OnboarderPageState();
 }
 
+class HolidayType {
+  final String asset;
+  final String name;
+
+  HolidayType(this.asset, this.name);
+}
+
 class _OnboarderPageState extends State<OnboarderPage> {
+  bool acceptedLicense = false;
+  final outerScrollController = PageController();
+  // final innerScrollController = PageController();
+  double innerScrollPosition = 0;
+  // final innerScrollController
+
+  final holidayTypes = [
+    HolidayType('buggy.jpg', 'Driving'),
+    HolidayType('cave_diving.jpg', 'Exploring'),
+    HolidayType('exploration.jpg', 'Discovery'),
+    HolidayType('rafting.jpg', 'Rafting'),
+    HolidayType('relaxation.jpg', 'Relaxation'),
+    HolidayType('water.jpg', 'Boating'),
+  ];
+
+  final selectedHolidayTypes = <String>{};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: true
-          ? EnticerOpenerPage()
-          : PageView(
-              children: [
-                OnboardStep(Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AutoSizeText(
-                        'WELCOME TO',
-                        style: TextStyle(fontSize: 50),
-
-                        // minFontSize: 20,
-                        maxLines: 1,
+      body: PageView(
+        controller: outerScrollController,
+        physics: NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        children: [
+          ClipRect(
+            child: EnticerOpenerPage(
+              getStartedPressed: () => outerScrollController.animateToPage(
+                1,
+                duration: Duration(seconds: 1),
+                curve: Curves.fastOutSlowIn,
+              ),
+            ),
+          ),
+          Stack(
+            children: [
+              PageView(
+                // controller: innerScrollController,
+                onPageChanged: (val) {
+                  setState(() {
+                    innerScrollPosition = val.toDouble();
+                  });
+                },
+                children: [
+                  OnboardStep(
+                    [
+                      Text(
+                        'Get ready for the trip of a lifetime.',
+                        style: Theme.of(context).textTheme.headline5,
+                        textAlign: TextAlign.center,
                       ),
-                      AutoSizeText(
-                        'SYNERGY',
-                        maxLines: 1,
-                        style: TextStyle(fontSize: 100),
+                      Text(
+                        'Synergy Holidays is a way for you to holiday, and really enjoy it.',
+                        textAlign: TextAlign.center,
                       ),
                     ],
+                    image: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Image.asset('assets/explore.png'),
+                    ),
                   ),
-                )),
-                OnboardStep(
-                  Text('Page two'),
+                  OnboardStep(
+                    [
+                      Text(
+                        'But first, we have to keep the lawyers happy.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Text(
+                        'Use of Synergy Travel is guided by the terms and conditions set out in the license agreement.',
+                        textAlign: TextAlign.center,
+                      ),
+                      CheckboxListTile(
+                        value: acceptedLicense,
+                        onChanged: (val) {
+                          setState(() {
+                            acceptedLicense = val ?? false;
+                          });
+                        },
+                        title: Text('I accept the license agreement'),
+                      )
+                    ],
+                    image: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Image.asset('assets/lawyers.png'),
+                    ),
+                  ),
+                  OnboardStep(
+                    [
+                      Text(
+                        'What do you like doing?',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      GridView.count(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        crossAxisCount: 2,
+                        children: [
+                          ...holidayTypes.map(
+                            (e) => AnimatedContainer(
+                              duration: Duration(milliseconds: 100),
+                              padding: selectedHolidayTypes.contains(e.name) ? EdgeInsets.all(16) : EdgeInsets.zero,
+                              child: Card(
+                                // borderOnForeground: true,
+                                semanticContainer: true,
+                                clipBehavior: Clip.antiAlias,
+                                child: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      if (selectedHolidayTypes.contains(e.name)) {
+                                        selectedHolidayTypes.remove(e.name);
+                                      } else {
+                                        selectedHolidayTypes.add(e.name);
+                                      }
+                                    });
+                                  },
+                                  child: Ink.image(
+                                    image: AssetImage(
+                                      'assets/holidaytypes/${e.asset}',
+                                    ),
+                                    fit: BoxFit.cover,
+                                    child: Stack(
+                                      alignment: Alignment.center,
+                                      fit: StackFit.expand,
+                                      children: [
+                                        Align(
+                                          child: Container(
+                                            padding: EdgeInsets.zero,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                e.name,
+                                                textAlign: TextAlign.center,
+                                              ),
+                                            ),
+                                            width: double.maxFinite,
+                                            color: Colors.white,
+                                          ),
+                                          alignment: Alignment.bottomCenter,
+                                        ),
+                                        AnimatedOpacity(
+                                          opacity: selectedHolidayTypes.contains(e.name) ? 1.0 : 0.0,
+                                          duration: Duration(milliseconds: 100),
+                                          child: Container(
+                                            height: double.maxFinite,
+                                            width: double.maxFinite,
+                                            color: Colors.grey.withOpacity(0.3),
+                                            child: Icon(
+                                              Icons.thumb_up_alt_outlined,
+                                              color: Colors.white,
+                                              size: 50,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  OnboardStep(
+                    [
+                      Text(
+                        "Pack your bags, you're on your way!",
+                        style: Theme.of(context).textTheme.headline5,
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "That's all the information we need for now. We'll start offering you great holidays tailored to you, once you tap the button below!",
+                        textAlign: TextAlign.center,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => SimpleDialog(
+                                title: Text(
+                                  "We'd love to, but...",
+                                ),
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                        "...this is just a demo. Feel free to take a holiday whenever you want, though."),
+                                  ),
+                                  TextButton(
+                                    child: Text("IT'S OKAY, I GET IT"),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                          child: Text('MATCH ME TO MY NEXT HOLIDAY'))
+                    ],
+                    image: Image.asset('assets/get_set.png'),
+                  )
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: DotsIndicator(
+                  dotsCount: 4,
+                  position: innerScrollPosition,
+                  // position: innerScrollController.hasClients ? innerScrollController.page ?? 0 : 0,
+                  decorator: DotsDecorator(
+                    size: const Size.square(9.0),
+                    activeSize: const Size(18.0, 9.0),
+                    activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                  ),
                 ),
-              ],
-            ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
 
 class OnboardStep extends StatelessWidget {
   final Widget? image;
-  final Widget text;
+  final List<Widget> children;
 
   OnboardStep(
-    this.text, {
+    this.children, {
     this.image,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueAccent,
+      color: Colors.blue.shade200,
+      // color: Colors.blueAccent,
       child: Column(
         children: [
           if (image != null)
             Expanded(
-              child: image!,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Card(
+                    elevation: 10,
+                    child: image!,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
+              ),
               flex: 2,
             ),
           Expanded(
-            child: Center(child: text),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: children,
+              ),
+            ),
             flex: 1,
           ),
         ],
       ),
     );
-    // TODO: implement build
-    // throw UnimplementedError();
   }
 }
